4790c0269845240269902c51253d4e52c8529615
Jade Byfield <jjbyfield@gmail.com>
2015-08-03 17:48:06 -0400

    Playing around with custom loading animation for Picasso q

--- app/src/main/java/relish/permoveo/com/relish/adapter/PlacesAdapter.java
+++ app/src/main/java/relish/permoveo/com/relish/adapter/PlacesAdapter.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
